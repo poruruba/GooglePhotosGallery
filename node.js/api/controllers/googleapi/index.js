b38f8b9ae0334b002ef04371977bcc10aafa0261
poruruba <44470781+poruruba@users.noreply.github.com>
2021-10-24 17:15:00 +0900

    Update index.js

--- node.js/api/controllers/googleapi/index.js
+++ node.js/api/controllers/googleapi/index.js
@@ -11,55 +11,57 @@ const CLIENT_SECRET = '【クライアントシークレット】';
 const API_KEY = "【適当なAPIキー】";
 
 exports.handler = async (event, context, callback) => {
-	if( event.path == '/googleapi-token'){
-		if (event.requestContext.apikeyAuth.apikey != API_KEY )
-			throw 'apikey mismatch';
+  if (event.path == '/googleapi-token') {
+    if (event.requestContext.apikeyAuth.apikey != API_KEY)
+      throw 'apikey mismatch';
 
-		var body = JSON.parse(event.body);
+    var body = JSON.parse(event.body);
 
     var params = {
-			code: body.code,
-			client_id: CLIENT_ID,
-			client_secret: CLIENT_SECRET,
-			redirect_uri: body.redirect_uri,
+      code: body.code,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      redirect_uri: body.redirect_uri,
       grant_type: "authorization_code",
     };
     var result = await do_post('https://www.googleapis.com/oauth2/v4/token', params);
-		console.log(result);
+    console.log(result);
 
-		return new Response(result);
-	}
-	if( event.path == '/googleapi-refreshtoken' ){
-		if (event.requestContext.apikeyAuth.apikey != API_KEY)
-			throw 'apikey mismatch';
+    return new Response(result);
+  }
+  if (event.path == '/googleapi-refreshtoken') {
+    if (event.requestContext.apikeyAuth.apikey != API_KEY)
+      throw 'apikey mismatch';
 
-		var body = JSON.parse(event.body);
-		console.log(body);
+    var body = JSON.parse(event.body);
+    console.log(body);
 
-		var params = {
-			refresh_token: body.refresh_token,
-			client_id: CLIENT_ID,
-			client_secret: CLIENT_SECRET,
-			grant_type: "refresh_token",
-		};
-		var result = await do_post('https://www.googleapis.com/oauth2/v4/token', params);
-		console.log(result);
+    var params = {
+      refresh_token: body.refresh_token,
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET,
+      grant_type: "refresh_token",
+    };
+    var result = await do_post('https://www.googleapis.com/oauth2/v4/token', params);
+    console.log(result);
 
-		return new Response(result);
-	}
+    return new Response(result);
+  }
 };
 
 function do_post(url, body) {
-	const headers = new Headers({ "Content-Type": "application/json; charset=utf-8" });
+  const headers = new Headers({
+    "Content-Type": "application/json; charset=utf-8"
+  });
 
-	return fetch(url, {
-		method: 'POST',
-		body: JSON.stringify(body),
-		headers: headers
-	})
-		.then((response) => {
-			if (!response.ok)
-				throw 'status is not 200';
-			return response.json();
-		});
+  return fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: headers
+    })
+    .then((response) => {
+      if (!response.ok)
+        throw 'status is not 200';
+      return response.json();
+    });
 }
